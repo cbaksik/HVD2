676af89cd308820333073b7f4b15068a18a7d1a8
Corinna Baksik <corinna_baksik@harvard.edu>
2019-12-05 15:42:40 -0500

    aligning with hvd_images

--- js/custom-routers.js
+++ js/custom-routers.js
@@ -1,63 +1,42 @@
 /**
- * Created by samsan on 11/21/17.
- * Implement ui-router so it can load new pages and new component
+ * Created by samsan on 3/20/18.
+ * List all custom routers for images component and images details
  */
 
 (function () {
-
     angular.module('viewCustom')
-    .config(function ($stateProvider) {
-        $stateProvider
-            .state('exploreMain.almaMapIt', {
-                    url: '/almaMapIt',
-                    views:{
-                        '': {
-                            template: `<custom-library-map loc="$ctrl"></custom-library-map>`
+        .config(function ($stateProvider) {
+            $stateProvider
+                .state('exploreMain.viewallcomponentdata', {
+                        url: '/viewallcomponentmetadata/:context/:docid',
+                        views:{
+                            '': {
+                                template: `<custom-view-all-component-metadata parent-ctrl="$ctrl"></custom-view-all-component-metadata>`
+                            }
                         }
                     }
-                }
 
-            )
-            .state('exploreMain.aeon', {
-                url: '/aeon/:mmsid',
-                    views:{
-                    '': {
-                        template: `<custom-aeon parent-ctrl="$ctrl"></custom-aeon>`
+                )
+                .state('exploreMain.viewcomponent', {
+                        url:'/viewcomponent/:context/:docid',
+                        views:{
+                            '':{
+                                template:`<custom-view-component parent-ctrl="$ctrl" item="$ctrl.item" services="$ctrl.services" params="$ctrl.params"></custom-view-component>`
+                            }
                         }
                     }
-                }
-            )
-            .state('exploreMain.viewallcomponentdata', {
-                    url: '/viewallcomponentmetadata/:context/:docid',
-                    views:{
-                        '': {
-                            template: `<custom-view-all-component-metadata parent-ctrl="$ctrl"></custom-view-all-component-metadata>`
+
+                )
+                .state('exploreMain.printPage', {
+                        url: '/printPage/:context/:docid',
+                        views:{
+                            '': {
+                                template: `<custom-print-page parent-ctrl="$ctrl"></custom-print-page>`
+                            }
                         }
                     }
-                }
 
-            )
-            .state('exploreMain.viewcomponent', {
-                    url:'/viewcomponent/:context/:docid',
-                    views:{
-                        '':{
-                            template:`<custom-view-component parent-ctrl="$ctrl" item="$ctrl.item" services="$ctrl.services" params="$ctrl.params"></custom-view-component>`
-                        }
-                    }
-                }
-
-            )
-            .state('exploreMain.printPage', {
-                    url: '/printPage/:context/:docid',
-                    views:{
-                        '': {
-                            template: `<custom-print-page parent-ctrl="$ctrl"></custom-print-page>`
-                        }
-                    }
-                }
-
-            )
-    });
-
+                )
+        });
 
 })();
