a778f8bfdd8bedf8f02d146d29ecdcf1c79607e7
Corinna Baksik <corinna_baksik@harvard.edu>
2019-12-05 16:21:36 -0500

    Revert "aligning with sam's changes to hvd_images"
    This reverts commit 0ea8b013f17bee24ed4f5d1af94c604c9f219a40.

--- js/custom-thumbnail.js
+++ js/custom-thumbnail.js
@@ -3,9 +3,7 @@
  * If image has height that is greater than 150 px, then it will resize it. Otherwise, it just display what it is.
  */
 
-(function () {
-
-    angular.module('viewCustom')
+angular.module('viewCustom')
     .component('customThumbnail', {
         templateUrl:'/primo-explore/custom/HVD2/html/custom-thumbnail.html',
         bindings: {
@@ -78,5 +76,3 @@
 
         }]
     });
-
-})();
