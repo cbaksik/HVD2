4de7551b1a610e7c14bb693175fdec57b5569adb
Corinna Baksik <corinna_baksik@harvard.edu>
2020-02-21 13:44:47 -0500

    mobile improvements with Feb release, inactive footer code

---
+++ js/prm-explore-footer-after.js
@@ -0,0 +1,21 @@
+/**
+ * Created by cbaksik on 1/29/20
+ * This component used to add a footer to site
+ * prm-explore-footer-after
+ */
+
+/* (function () {
+    angular.module('viewCustom')
+        .controller('prmExploreFooterAfterCtrl',[function() {
+            var vm=this;        
+            
+        }]);
+
+    angular.module('viewCustom')
+        .component('prmExploreFooterAfter',{
+            bindings:{parentCtrl:'<'},
+            controller: 'prmExploreFooterAfterCtrl',
+            templateUrl:'/primo-explore/custom/HVD2/html/prm-explore-footer-after.html'
+        });
+
+})(); */
