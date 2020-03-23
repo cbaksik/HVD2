6063cd5852ee54b130cdea7d78e59e7bc1f47bd9
Corinna Baksik <corinna_baksik@harvard.edu>
2020-03-23 12:16:14 -0400

    new blurb for open access during pandemic

---
+++ js/prm-loans-overview-after.js
@@ -0,0 +1,21 @@
+/**
+ * Created by cbaksik on 3/18/20.
+ * This component used to add note to loans/renew because of covid extension
+ */
+
+/* (function () {
+    angular.module('viewCustom')
+        .controller('prmLoansOverviewAfterCtrl',[function() {
+            var vm=this;        
+            
+        }]);
+
+    angular.module('viewCustom')
+        .component('prmLoansOverviewAfter',{
+            bindings:{parentCtrl:'<'},
+            controller: 'prmLoansOverviewAfterCtrl',
+            templateUrl:'/primo-explore/custom/HVD2/html/prm-loans-overview-after.html'
+            //template: '<h2>TESTING</h2>'
+        });
+
+})(); */
