8ea7b99eee69f53a7e4ad1b874a2464e422cb353
Corinna Baksik <corinna_baksik@harvard.edu>
2020-12-04 16:06:24 -0500

    new search hathi link for links area

--- js/prm-service-links-after.js
+++ js/prm-service-links-after.js
@@ -14,11 +14,32 @@
         vm.recordLinks=[]; // keep track the original vm.parentCtrl.recordLinks
         vm.getData=()=> {
             // make a copy to avoid data binding
-            vm.recordLinks = angular.copy(vm.parentCtrl.recordLinks);            
-            var searchHathiBase = 'https://catalog.hathitrust.org/Search/Home?lookfor=';
-            var searchHathiLinkAppend = '%26urlappend=%3Bsignon=swle:https://fed.huit.harvard.edu/idp/shibboleth';
-            vm.searchHathiLink= searchHathiBase + vm.parentCtrl.item.pnx.addata.btitle[0] + '%26type=title' + searchHathiLinkAppend;
-            //vm.btitle = vm.addata.btitle[0];       
+            vm.recordLinks = angular.copy(vm.parentCtrl.recordLinks);        
+            /* START hathi section */    
+            var searchHathiBase = 'https://catalog.hathitrust.org/Search/Home?adv=1&lookfor%5B%5D=';
+            var searchHathiLinkAppend = '&urlappend=%3Bsignon=swle:https://fed.huit.harvard.edu/idp/shibboleth';
+            //console.log(vm.parentCtrl.item.pnx.addata);
+            console.log(vm.parentCtrl.item.pnx.display.type[0]);
+            // only present hathi link for book, music, journal
+            var format = vm.parentCtrl.item.pnx.display.type[0];  
+            var title = vm.parentCtrl.item.pnx.display.title[0]; 
+            var author = '';
+            var year = '';            
+            if (vm.parentCtrl.item.pnx.addata.aulast) {
+                author = vm.parentCtrl.item.pnx.addata.aulast[0];
+            }
+            if (!format == 'journal' && !author && vm.parentCtrl.item.pnx.addata.addau) {
+                author = vm.parentCtrl.item.pnx.addata.addau[0];
+            }
+            if (vm.parentCtrl.item.pnx.addata.risdate){
+                year = vm.parentCtrl.item.pnx.addata.risdate[0];       
+            }                       
+            if (format == 'book' || format == 'journal' || format == 'music') {
+                vm.showHathiLink = "true";                
+            }
+            //console.log(vm.showHathiLink );
+            vm.searchHathiLink = searchHathiBase + author + '&type%5B%5D=author&bool%5B%5D=AND&lookfor%5B%5D='  + title + '&type%5B%5D=title&bool[]=AND&lookfor[]=' + year + '&type[]=year&bool[]=AND&yop=after' + searchHathiLinkAppend;  
+            /* END hathi section */                  
             // get items that have digital bookplates
             vm.itemList=cisv.extractImageUrl(vm.parentCtrl.item, vm.recordLinks);
             // delay data from parentCtrl
