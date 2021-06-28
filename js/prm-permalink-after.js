513a6dbffbd9242794fcbc1a98c6e5ee0b7ca9c1
Corinna Baksik <cbaksik@gmail.com>
2021-06-28 15:19:07 -0400

    altering permalink so that code only runs if pnx has our local service in lds03
    changing this b/c customization was also overwriting native short links for CDI content

--- js/prm-permalink-after.js
+++ js/prm-permalink-after.js
@@ -8,31 +8,26 @@ angular.module('viewCustom')
         vm.msg={'class':''};
         vm.$onInit=function () {
             vm.permalinkText='';
-            // change perm a link to correct url
+            // change perm a link to correct url, only for data where we've put a link to id.lib via norm rules
             $scope.$watch('vm.parentCtrl.permalink',function () {
-                if(vm.parentCtrl.item){
-                    if(vm.parentCtrl.item.pnx.display.lds03) {
-                        vm.parentCtrl.permalink = vm.parentCtrl.item.pnx.display.lds03[0];
-                    }
-                }
-                if(vm.parentCtrl.permalink) {
-                    vm.permalink=$sce.trustAsHtml(vm.parentCtrl.permalink);
+                if(vm.parentCtrl.item && vm.parentCtrl.item.pnx.display.lds03){                    
+                        vm.parentCtrl.permalink = vm.parentCtrl.item.pnx.display.lds03[0];                        
+                        vm.permalink=$sce.trustAsHtml(vm.parentCtrl.permalink);
+                        console.log(vm.permalink.innerHtml);
                     // remove parent node
                     var pNode=$element[0].parentNode.children[0];
 
                     if(pNode) {
                        pNode.style.display='none';
-                    }
+                    } 
                     // get link text
                     setTimeout(()=>{
                         var el=$element[0].children[0].children[0].children[0].children[0].children[0].children[0];
                         if(el) {
                             vm.permalinkText=el.textContent;
                         }
-                    },1000);
-
+                    },1000);                
                 }
-                
             });
 
 
