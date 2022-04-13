36790513f54c3aa215f1b2577d6fde9f0c46be04
mferrarini <maura_ferrarini@harvard.edu>
2022-04-13 10:40:24 -0400

    Footer delayed 5 seconds for full record view pages to give other page content time to load

--- js/prm-explore-footer-after.js
+++ js/prm-explore-footer-after.js
@@ -1,18 +1,25 @@
 /**
  * Created by mferrarini on 2/9/22.
  * Standard HL footer
+ * Footer delayed 5 seconds for full record view pages to give other page content time to load
  */
 
 angular.module('viewCustom')
 .controller('prmExploreFooterAfterCtrl',[function () {
 		var vm=this;
-		vm.$onInit=function () {
-			setTimeout(()=>{
-				const showFooter = angular.element(document.querySelector('footer'));
-				showFooter.addClass("display");
-			}, 2000)
-		};
+		vm.footer = angular.element(document.querySelector('footer'));
 
+		var exploreMain = angular.element(document.querySelector('prm-explore-main')); // main content block on all pages except full record view pages
+
+		vm.$onInit=function () {
+			if (exploreMain.length) {
+				vm.footer.addClass("display");
+			} else {
+				setTimeout(()=>{
+					vm.footer.addClass("display");
+				}, 5000)
+			}
+		};
 }]);
 
 angular.module('viewCustom')
