e456e0685fe52c2e4ff4c2c7cc7c01501908b06e
mferrarini <maura_ferrarini@harvard.edu>
2022-05-11 16:03:57 -0400

    Switched to using GTM

--- js/custom-google-analytics.js
+++ js/custom-google-analytics.js
@@ -23,36 +23,24 @@
     }
 
 
-    // Google Analytics
-    const trackingId_GA4 = 'G-3CXC97RWEK';
-
-    const defaultCode = `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '${trackingId_GA4}');`;
-
-    const defaultURL = `https://www.googletagmanager.com/gtag/js?id=${trackingId_GA4}`;
-
-    addExternalScript(defaultURL);
-    addInlineScript(defaultCode);
-
-
+    // Google Analytics 4
+    // Added as variable/constant in GTM
+    // const trackingId_GA4 = 'G-3CXC97RWEK';
+ 
 
     // Google Tag Manager
-    // const trackingId_GTM= 'GTM-1234567';
+    const trackingId_GTM = 'GTM-WSHSDQ5';
 
-    // const gtmHeadCode =
-    //     `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    //     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    //     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    //     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    //     })(window,document,'script','dataLayer','${trackingId_GTM}');`;
+    const gtmHeadCode =
+        `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${trackingId_GTM}');`;
 
-    // const gtmBodyCode = `<iframe src="https://www.googletagmanager.com/ns.html?id=${trackingId_GTM}"
-    //     height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+    const gtmBodyCode = `<iframe src="https://www.googletagmanager.com/ns.html?id=${trackingId_GTM}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
 
-    // addInlineScript(gtmHeadCode);
-    // addInlineScript(gtmBodyCode, 'noscript', 'body');
+    addInlineScript(gtmHeadCode);
+    addInlineScript(gtmBodyCode, 'noscript', 'body');
 })();
