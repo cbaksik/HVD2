0ec0ece1f7cf8bb915e076c69e760fd29f50e954
Corinna Baksik <cbaksik@gmail.com>
2022-07-15 14:15:08 -0400

    correcting linke to help, appending referrer to broken link menu item, uppercase HOLLIS

--- js/prm-logo-after.js
+++ js/prm-logo-after.js
@@ -6,8 +6,17 @@
 angular.module('viewCustom')
     .controller('prmLogoAfterCtrl',[function () {
         var vm=this;
+        var brokenLink = '';        
+        var currentURL = '';
+        //var pageTitle = '';
         vm.$onInit=function () {
-
+            brokenLink = document.querySelectorAll("a[href^='https://asklib-its-harvard.libanswers.com/form?queue_id=6017']");
+            //pageTitle = document.getElementById('ogTitle').getAttribute('content');            
+            //console.log(brokenLink[0].href);
+            currentURL = encodeURIComponent(window.location.href);
+            //pageTitle = encodeURIComponent(window.document.title);
+            //console.log(pageTitle);
+            brokenLink[0].href = brokenLink[0].href + '&referrer=' + currentURL;
         };
 
     }]);
