cc8722ce19ed911239b59edb058b956c5c2690a2
Corinna Baksik <cbaksik@gmail.com>
2022-12-01 13:51:22 -0500

    new domain per libanswers, for chat widget on home page

--- js/custom-chatwidget.js
+++ js/custom-chatwidget.js
@@ -9,7 +9,7 @@
 
     var chat = document.createElement('script'); chat.type = 'text/javascript'; chat.async = 'true';
 
-    chat.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'v2.libanswers.com/load_chat.php?hash=10372f999a1c3953ffde50858ead4611';
+    chat.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'ask.library.harvard.edu/load_chat.php?hash=10372f999a1c3953ffde50858ead4611';
 
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(chat, s);
 
