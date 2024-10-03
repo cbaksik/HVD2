163aa4ccbee89e76393dadf8a50ef432e4d9d58e
Corinna Baksik <cbaksik@gmail.com>
2024-10-03 13:09:53 -0400

    alma digital aux pilot
    if bib has 919 field then alma-d thumbnail will be used and that will trigger new 'sample content' section that embeds universal viewer

--- js/prm-service-links-after.js
+++ js/prm-service-links-after.js
@@ -7,12 +7,11 @@
 (function(){
     'use strict';
     angular.module('viewCustom')
-    .controller('prmServiceLinksAfterCtrl',['customImagesService','$timeout',function (customImagesService, $timeout) {
+    .controller('prmServiceLinksAfterCtrl',['customImagesService','$timeout','$sce',function (customImagesService, $timeout,$sce) {
         let vm=this;
         let cisv=customImagesService;
         vm.itemList=[];
         vm.almaDaux='';
-        vm.almaDaux2='';
         vm.recordLinks=[]; // keep track the original vm.parentCtrl.recordLinks
         vm.getData=()=> {
             // make a copy to avoid data binding
@@ -43,13 +42,18 @@
             vm.searchHathiLink = searchHathiBase + author + '&type%5B%5D=author&bool%5B%5D=AND&lookfor%5B%5D='  + title + '&type%5B%5D=title&bool[]=AND&lookfor[]=' + year + '&type[]=year&bool[]=AND&yop=after' + searchHathiLinkAppend;  
             /* END hathi section */   
 
-            /* TESTING ALMA-D AUX IMAGE DISPLAY */
-            if (vm.parentCtrl.item.pnx.search.lsr01[0] == '990007664060203941') {
-                vm.almaDaux = '990007664060203941';
-                //console.log("dress for success");
-            }
-            if (vm.parentCtrl.item.pnx.search.lsr01[0] == '990140136830203941') {
-                vm.almaDaux2 = '990140136830203941';
+            /* ALMA-D AUX IMAGE DISPLAY */
+
+
+
+            if (vm.parentCtrl.item.pnx.links.thumbnail[0].includes('hvd.alma.exlibrisgroup.com/view/delivery/thumbnail/01HVD_INST/12')) {
+                vm.almaDaux = vm.parentCtrl.item.pnx.links.thumbnail[0].substring(73);
+                vm.almaDauxEmbed = 'https://hvd.alma.exlibrisgroup.com/view/UniversalViewer/01HVD_INST/' + vm.almaDaux + '#?iiifVersion=3&updateStatistics=false&embedded=true&c=0&m=0&s=0&cv=0&config=&locales=en-GB:English (GB),cy-GB:Cymraeg,fr-FR:Français (FR),pl-PL:Polski,sv-SE:Svenska,xx-XX:English (GB) (xx-XX)&r=0';
+                //console.log(vm.almaDaux);
+                //console.log(vm.almaDauxEmbed);
+                
+                vm.almaDauxEmbed = $sce.trustAsResourceUrl(vm.almaDauxEmbed);
+
             }
 
 
